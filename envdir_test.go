9bb021cb55a9b1c7fde5cdfae36fdf161ccaf6f5
Yuuki Furuyama <addsict@gmail.com>
2017-11-26 16:19:51 +0900

    Add more test

--- envdir_test.go
+++ envdir_test.go
@@ -6,15 +6,27 @@ import (
 )
 
 func TestEnvdir(t *testing.T) {
-	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
-	e := &Envdir{outStream: outStream, errStream: errStream}
+	t.Run("simple case", func(t *testing.T) {
+		outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+		e := &Envdir{outStream: outStream, errStream: errStream}
 
-	status := e.Run([]string{"envdir", "testenv", "sh", "-c", "printf %s $FOO"})
-	if status != ExitCodeOk {
-		t.Errorf("expected %d, but got %d", ExitCodeOk, status)
-	}
+		status := e.Run([]string{"envdir", "testenv", "sh", "-c", "printf %s $FOO"})
+		if status != 0 {
+			t.Errorf("expected %d, but got %d", 0, status)
+		}
 
-	if outStream.String() != "foo" {
-		t.Errorf("expected %q, but got %q", "foo", outStream.String())
-	}
+		if outStream.String() != "foo" {
+			t.Errorf("expected %q, but got %q", "foo", outStream.String())
+		}
+	})
+
+	t.Run("command exit with error", func(t *testing.T) {
+		outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+		e := &Envdir{outStream: outStream, errStream: errStream}
+
+		status := e.Run([]string{"envdir", "testenv", "sh", "-c", "exit 3"})
+		if status != 3 {
+			t.Errorf("expected %d, but got %d", 3, status)
+		}
+	})
 }
