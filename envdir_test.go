7ed5cdfe86827e18025c7aa07c698294f4b82214
Yuuki Furuyama <addsict@gmail.com>
2017-11-26 16:41:54 +0900

    Add test skelton

--- envdir_test.go
+++ envdir_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestEnvdir(t *testing.T) {
+func TestEnvdir_success(t *testing.T) {
 	t.Run("simple case", func(t *testing.T) {
 		outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
 		e := &Envdir{outStream: outStream, errStream: errStream}
@@ -18,6 +18,39 @@ func TestEnvdir(t *testing.T) {
 		if outStream.String() != "foo" {
 			t.Errorf("expected %q, but got %q", "foo", outStream.String())
 		}
+	})
+
+	t.Run("env file size is 0, so remove that env", func(t *testing.T) {
+		// TODO
+	})
+
+	t.Run("the end of space in env value is removed", func(t *testing.T) {
+		// TODO
+	})
+
+	t.Run("the end of tab in env value is removed", func(t *testing.T) {
+		// TODO
+	})
+}
+
+func TestEnvdir_error(t *testing.T) {
+	t.Run("less arguments provided", func(t *testing.T) {
+		outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+		e := &Envdir{outStream: outStream, errStream: errStream}
+
+		status := e.Run([]string{"envdir"})
+		if status != 111 {
+			t.Errorf("expected %d, but got %d", 111, status)
+		}
+
+		status = e.Run([]string{"envdir", "testenv"})
+		if status != 111 {
+			t.Errorf("expected %d, but got %d", 111, status)
+		}
+	})
+
+	t.Run("dir must not contain inner directory", func(t *testing.T) {
+		// TODO
 	})
 
 	t.Run("command exit with error", func(t *testing.T) {
